fc7366520911aa669751c0cd1ed7f5f109884716
Family <Family@192.168.1.160>
2023-12-16 20:27:56 +0100

    cin cout

--- src/CPPExamples.cpp
+++ src/CPPExamples.cpp
@@ -10,6 +10,16 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	int favorite_number;
+
+	std::cout << "Enter your favorite number between 1 and 100: ";
+
+	std::cin >> favorite_number;
+
+	std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+
+	std::cout << "No really!!, " << favorite_number << " is my favorite number!"
+			<< std::endl;
+
 	return 0;
 }
